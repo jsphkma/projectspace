082244f8181486451c50ed11dd2e7f7a61b82206
Tomnguyen2604 <tomnguyen2604@gmail.com>
2023-09-24 00:47:55 -0400

    Update on the file. Change the Vite.js into Expo

---
+++ assets/scripts/siminta.js
@@ -0,0 +1,54 @@
+$(function () {
+    // tooltip demo
+    $('.tooltip-demo').tooltip({
+        selector: "[data-toggle=tooltip]",
+        container: "body"
+    })
+
+    // popover demo
+    $("[data-toggle=popover]")
+        .popover()
+    ///calling side menu
+
+    $('#side-menu').metisMenu();
+
+    ///pace function for showing progress
+
+    function load(time) {
+        var x = new XMLHttpRequest()
+        x.open('GET', "" + time, true);
+        x.send();
+    };
+
+    load(20);
+    load(100);
+    load(500);
+    load(2000);
+    load(3000);
+    setTimeout(function () {
+        Pace.ignore(function () {
+            load(3100);
+        });
+    }, 4000);
+
+    Pace.on('hide', function () {
+        console.log('done');
+    });
+    paceOptions = {
+        elements: true
+    };
+   
+
+});
+
+//Loads the correct sidebar on window load, collapses the sidebar on window resize.
+$(function() {
+    $(window).bind("load resize", function() {
+        console.log($(this).width())
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+        }
+    })
+})
