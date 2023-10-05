b30007b460e5c9849f9c22b8737b8feeecff2423
Tomnguyen2604 <tomnguyen2604@gmail.com>
2023-10-05 17:08:30 -0400

    Trying to fix on the bug 10/05/2023

---
+++ client/src/pages/base/Login.js
@@ -0,0 +1,14 @@
+export const Login = () => {
+    return (
+      <section className='home'>
+        <img src={HeroImg} alt='' className='bg-img' />
+        <div className='container'>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem eos
+            facere, debitis voluptatum illum ipsum enim necessitatibus commodi
+            obcaecati sequi.
+          </h2>
+        </div>
+      </section>
+    );
+  };
---
+++ client/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
