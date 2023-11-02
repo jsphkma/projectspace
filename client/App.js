c1d53b17f0b6cc1c11f56368b3dc825f1d649123
Tomnguyen2604 <tomnguyen2604@gmail.com>
2023-11-02 07:27:49 -0400

    Update on the css of the Login page
    11/02/2023

--- client/App.js
+++ client/App.js
@@ -1,7 +1,13 @@
 import Login from './src/pages/Login';
 import React from 'react';
-function App() {
+import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
   return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style="auto" />
       <Login />
-)}; 
-export default App;
+    </View>
+  );
+}
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,24 +1,98 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('https://your-server.com/login', {
+        username,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <div className='wrapper'>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)}/>
-        <input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-        <button type="submit">Login</button>
-        <button type="forgot">Forgot Password</button>
-      </form>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.logo}>Login</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleSubmit}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    color: '#fb5b5a',
+    marginBottom: 40,
+  },
+  inputView: {
+    width: '100%',
+    backgroundColor: '#fff',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: 'black',
+  },
+  forgot: {
+    color: 'black',
+    fontSize: 16,
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: 'white',
+  },
+});
 
 export default LoginPage;
